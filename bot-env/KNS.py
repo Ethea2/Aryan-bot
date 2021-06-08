3f0da68ce5d5c98cafb2b87dbf6750a63716689a
Ethea2 <nathandres2001@gmail.com>
2021-06-08 13:04:12 +0400

    new(privetized) token

--- bot-env/KNS.py
+++ bot-env/KNS.py
@@ -20,6 +20,6 @@ for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
-token = open("token.gitignore", 'r')
+token = open(".gitignore", 'r')
 
 client.run(token.read())
