6c1a8bb0e68504b4a17f47e6cd67796b0bcc40cc
Ethea2 <nathandres2001@gmail.com>
2021-06-10 22:53:14 +0400

    Added secret rock paper scissors command, last update for a long time

--- bot-env/cogs/bato bato pick.py
+++ bot-env/cogs/bato bato pick.py
@@ -71,7 +71,7 @@ class rock_paper_scissors(commands.Cog):
 				random_attack = random.randint(0,2)
 				attack = my_turn[random_attack]
 				if attack == my_turn[0]:
-					await ctx.send(msg + " MY CHOICE IS: {0} {1} \nYOU WIN THIS TIME NOOB :smiling_imp:!".format(emoji[0],my_turn[0]))
+					await ctx.send(msg + " MY CHOICE IS: {0} {1} \nI WIN THIS TIME NOOB :smiling_imp:!".format(emoji[0],my_turn[0]))
 				elif attack == my_turn[1]:
 					await ctx.send(msg + " MY CHOICE IS: {0} {1} \nGFDI WHY DID WE TIE :unamused:!?!".format(emoji[1],my_turn[1]))
 				elif attack == my_turn[2]:
@@ -85,8 +85,25 @@ class rock_paper_scissors(commands.Cog):
 					await ctx.send(msg + " MY CHOICE IS: {0} {1} \nYOU'RE TOO EZ, I WIN :joy:!".format(emoji[1],my_turn[1]))
 				elif attack == my_turn[2]:
 					await ctx.send(msg + " MY CHOICE IS: {0} {1} \nWHY WOULD YOU PICK THAT? NOW WE TIED GRRR :confounded:!".format(emoji[2],my_turn[2]))
+			elif choice == 'nuke'.casefold():
+				await ctx.send(msg + ":mega:WTF WE'RE GETTING NUKED:mega:")
+				await ctx.send(msg + ":three:")
+				await ctx.send(msg + ":two:")
+				await ctx.send(msg + ":one:")
+				await ctx.send(msg + ":exploding_head::exploding_head::exploding_head::exploding_head:")
+			elif choice == 'tractor'.casefold():
+				await ctx.send(msg + "WTF ARE YOU GROWING PLANTS NOW?")
+				await ctx.send(msg + ":tractor:")
+				await ctx.send(msg + ":four_leaf_clover:")
+				await ctx.send(msg + ":herb:")
+				await ctx.send(msg + ":palm_tree:")
+				await ctx.send(msg + ":christmas_tree:")
+				await ctx.send(msg + "GREAT YOU GREW A CHRISTMAS TREE WTF")
+			
 			else:
 				await ctx.send(msg + " u gotta pick something scrub \nRock \nPaper \nScissors")
+
+
 
 def setup(client):
 	client.add_cog(rock_paper_scissors(client))
